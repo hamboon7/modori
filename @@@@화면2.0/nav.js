586d0a627b678620d693b9b9489d825b3737ea1c
ghdisgh <thm0420@naver.com>
2020-01-27 17:49:01 +0900

    Delete nav.js

--- @@@@화면2.0/nav.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function scrollNav() {
-  $('i').click(function(){  
-    //Toggle Class
-    $(".activeee").removeClass("activeee");      
-    $(this).addClass("activeee");
-  });  
-});                 
